8b4b23dcfe5e37995d6d8a00abc01df79628bb58
Adamouization <aj645@bath.ac.uk>
2018-03-19 19:52:42 +0000

    Add logo to header + dynamically load header

--- scripts/footer-loader.js
+++ scripts/footer-loader.js
@@ -9,3 +9,12 @@ function loadFooter() {
         $("#footer").load("../shared/footer-pages.html");
     }
 }
+
+function loadHeader() {
+    var fileName = getPageFilename();
+    if (fileName.indexOf("index") !== -1) {
+        $("#header").load("views/shared/header-home.html");
+    } else {
+        $("#header").load("../shared/header-pages.html");
+    }
+}
