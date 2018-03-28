2d8b13d22654c351e92af66da3c04e42482f98a1
Adam Jaamour <ajaamour@localhost.localdomain>
2018-03-28 13:54:27 +0100

    Add a popup to display tracklists for mixtapes

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -7,3 +7,29 @@ function getPageFilename() {
     var index = pathSplit.length - 1;
     return pathSplit[index];
 }
+
+/**
+ * When the user clicks on <div>, open the popup
+ */
+function showPopup(popupId) {
+    var key = "";
+    switch (popupId) {
+        case 'mixtape1button':
+            key = 'mixtape1';
+            break;
+        case 'mixtape2button':
+            key = 'mixtape2';
+            break;
+        case 'mixtape3button':
+            key = 'mixtape3';
+            break;
+        default:
+            key = "empty";
+    }
+    if (key !== 'empty') {
+        var popup = document.getElementById(key);
+        popup.classList.toggle("show");
+    } else {
+        // todo - add an error message
+    }
+}
